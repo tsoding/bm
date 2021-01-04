93bd7ab82d1bf5fe705ff8a81815fb9103840e3e
rexim <reximkut@gmail.com>
2021-01-04 21:05:40 +0700

    Add TODO(#39)

--- src/bm.h
+++ src/bm.h
@@ -31,7 +31,7 @@ typedef enum {
 const char *err_as_cstr(Err err);
 
 // TODO(#38): comparison instruction set is not complete
-// TODO: there is no operations for converting integer->float/float->interger
+// TODO(#39): there is no operations for converting integer->float/float->interger
 typedef enum {
     INST_NOP = 0,
     INST_PUSH,
