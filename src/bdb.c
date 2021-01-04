60209eb899e7e9e863849fc6b42c02ee6029bcbd
Nico Sonack <nsonack@outlook.com>
2021-01-04 19:29:13 +0100

    Implement breakpoints
    Not really working correctly yet

--- src/bdb.c
+++ src/bdb.c
@@ -36,19 +36,103 @@ void bdb_print_instr(FILE *f, Inst *i)
     }
 }
 
+void bdb_add_breakpoint(bdb_state *state, Inst_Addr addr)
+{
+    if (addr > state->bm.program_size)
+    {
+        fprintf(stderr, "ERR : Symbol out of program\n");
+        return;
+    }
+
+    if (addr > BM_PROGRAM_CAPACITY)
+    {
+        fprintf(stderr, "ERR : Symbol out of memory\n");
+        return;
+    }
+
+    if (state->breakpoints[addr].is_enabled)
+    {
+        fprintf(stderr, "ERR : Breakpoint already set\n");
+        return;
+    }
+
+    state->breakpoints[addr].is_enabled = 1;
+}
+
+void bdb_delete_breakpoint(bdb_state *state, Inst_Addr addr)
+{
+    if (addr > state->bm.program_size)
+    {
+        fprintf(stderr, "ERR : Symbol out of program\n");
+        return;
+    }
+
+    if (addr > BM_PROGRAM_CAPACITY)
+    {
+        fprintf(stderr, "ERR : Symbol out of memory\n");
+        return;
+    }
+
+    if (!state->breakpoints[addr].is_enabled)
+    {
+        fprintf(stderr, "ERR : No such breakpoint\n");
+        return;
+    }
+
+    state->breakpoints[addr].is_enabled = 0;
+}
+
+bdb_status bdb_continue(bdb_state *state)
+{
+    if (state->bm.halt)
+    {
+        fprintf(stderr, "ERR : Program is not being run\n");
+        return BDB_OK;
+    }
+
+    while (!state->bm.halt)
+    {
+        if (state->breakpoints[state->bm.ip].is_enabled)
+        {
+            fprintf(stdout, "Hit breakpoint at %"PRIu64"\n", state->bm.ip);
+            return BDB_OK;
+        }
+
+        Err err = bm_execute_inst(&state->bm);
+        if (err)
+        {
+            return bdb_fault(state, err);
+        }
+    }
+
+    return BDB_OK;
+}
+
+bdb_status bdb_fault(bdb_state *state, Err err)
+{
+    fprintf(stderr, "%s at %" PRIu64 " (INSTR: ",
+            err_as_cstr(err), state->bm.ip);
+    bdb_print_instr(stderr, &state->bm.program[state->bm.ip]);
+    fprintf(stderr, ")\n");
+    state->bm.halt = 1;
+    return BDB_OK;
+}
+
 bdb_status bdb_step_instr(bdb_state *state)
 {
+    if (state->bm.halt)
+    {
+        fprintf(stderr, "ERR : Program is not being run\n");
+        return BDB_OK;
+    }
+
     Err err = bm_execute_inst(&state->bm);
     if (!err)
     {
         return BDB_OK;
     }
 
-    fprintf(stderr, "%s at %" PRIu64 " (INSTR: ",
-            err_as_cstr(err), state->bm.ip);
-    bdb_print_instr(stderr, &state->bm.program[state->bm.ip]);
-    fprintf(stderr, ")\n");
-    return BDB_FAIL;
+    return bdb_fault(state, err);
 }
 
 int main(int argc, char **argv)
@@ -64,6 +148,7 @@ int main(int argc, char **argv)
      */
 
     bdb_state state = {0};
+    state.bm.halt = 1;
 
     printf("BDB - The birtual machine debugger.\n");
     bdb_state_init(&state, argv[1], argv[2]);
@@ -105,11 +190,47 @@ int main(int argc, char **argv)
         {
             bm_dump_stack(stdout, &state.bm);
         } break;
+        case 'b':
+        {
+            char *addr = input_buf + 2;
+            Inst_Addr break_addr = atoi(addr);
+            bdb_add_breakpoint(&state, break_addr);
+        } break;
+        case 'd':
+        {
+            char *addr = input_buf + 2;
+            Inst_Addr break_addr = atoi(addr);
+            bdb_delete_breakpoint(&state, break_addr);
+        } break;
+        case 'r':
+        {
+            if (!state.bm.halt)
+            {
+                fprintf(stderr, "ERR : Program is already running\n");
+                /* TODO: Reset bm and restart program */
+            }
+
+            state.bm.halt = 0;
+        } /* fall through */
+        case 'c':
+        {
+            if (bdb_continue(&state))
+            {
+                return EXIT_FAILURE;
+            }
+        } break;
         case EOF:
         case 'q':
         {
             return EXIT_SUCCESS;
         }
+        case 'h':
+        {
+            printf("n - next instruction\n"
+                   "s - stack dump\n"
+                   "i - instruction pointer\n"
+                   "q - quit\n");
+        } break;
         default:
         {
             fprintf(stderr, "?\n");
--- src/bdb.h
+++ src/bdb.h
@@ -5,18 +5,30 @@
 
 typedef enum {
    BDB_OK = 0,
-   BDB_FAIL = 1
+   BDB_FAIL = 1,
+   BDB_EXIT
 } bdb_status;
+
+typedef struct bdb_breakpoint
+{
+    int is_enabled;
+    int id;
+} bdb_breakpoint;
 
 typedef struct bdb_state
 {
     Bm bm;
     String_View cood_file_name;
     String_View symtab_file_name;
+    bdb_breakpoint breakpoints[BM_PROGRAM_CAPACITY];
 } bdb_state;
 
 void bdb_state_init(bdb_state *, const char *executable, const char *symtab);
 bdb_status bdb_step_instr(bdb_state *);
+bdb_status bdb_continue(bdb_state *);
 void bdb_print_instr(FILE *, Inst *);
+void bdb_add_breakpoint(bdb_state *, Inst_Addr);
+void bdb_delete_breakpoint(bdb_state *, Inst_Addr);
+bdb_status bdb_fault(bdb_state *, Err);
 
 #endif // BDB_H
