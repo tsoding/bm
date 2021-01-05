e67a05a3989dfa6139364fdd7954023d31bafa4d
rexim <reximkut@gmail.com>
2021-01-05 17:56:03 +0700

    (#8) Replace Makefile with ./build.sh

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -xe
+
+CC=${CC:=/usr/bin/cc}
+CFLAGS="-Wall -Wextra -Wswitch-enum -Wmissing-prototypes -pedantic -std=c11"
+LIBS=
+
+$CC $CFLAGS -o basm ./src/basm.c $LIBS
+$CC $CFLAGS -o bme ./src/bme.c $LIBS
+$CC $CFLAGS -o debasm ./src/debasm.c $LIBS
+
+for example in `find examples/ -name \*.basm | sed "s/\.basm//"`; do
+    ./basm "$example.basm" "$example.bm"
+done
