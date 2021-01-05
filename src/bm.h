7f1fad33a1eeb6894e4740cc25100c38e8356f61
rexim <reximkut@gmail.com>
2021-01-06 02:04:27 +0700

    Remove sv_to_int
    Nothing uses it anymore

--- src/bm.h
+++ src/bm.h
@@ -34,7 +34,6 @@ String_View sv_trim_right(String_View sv);
 String_View sv_trim(String_View sv);
 String_View sv_chop_by_delim(String_View *sv, char delim);
 int sv_eq(String_View a, String_View b);
-int sv_to_int(String_View sv);
 
 typedef enum {
     ERR_OK = 0,
@@ -734,17 +733,6 @@ int sv_eq(String_View a, String_View b)
     } else {
         return memcmp(a.data, b.data, a.count) == 0;
     }
-}
-
-int sv_to_int(String_View sv)
-{
-    int result = 0;
-
-    for (size_t i = 0; i < sv.count && isdigit(sv.data[i]); ++i) {
-        result = result * 10 + sv.data[i] - '0';
-    }
-
-    return result;
 }
 
 void *basm_alloc(Basm *basm, size_t size)
