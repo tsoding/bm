fc19a3c31f59924f8db91c780b5d155211afcf27
rexim <reximkut@gmail.com>
2021-01-11 19:47:39 +0700

    (#38) Take into account word boundaries when highlighting basm

--- tools/basm-mode.el
+++ tools/basm-mode.el
@@ -42,7 +42,7 @@
 (setq basm-highlights
       '(("%[[:word:]_]+" . font-lock-preprocessor-face)
         ("[[:word:]_]+\\:" . font-lock-constant-face)
-        ("nop\\|push\\|drop\\|dup\\|plusi\\|minusi\\|multi\\|divi\\|modi\\|plusf\\|minusf\\|multf\\|divf\\|jmp_if\\|jmp\\|eqi\\|halt\\|swap\\|not\\|gef\\|gei\\|ret\\|call\\|native\\|andb\\|orb\\|xor\\|shr\\|shl\\|notb\\|read8\\|read16\\|read32\\|read64\\|write8\\|write16\\|write32\\|write64" . font-lock-keyword-face)))
+        ("\\<\\(nop\\|push\\|drop\\|dup\\|plusi\\|minusi\\|multi\\|divi\\|modi\\|plusf\\|minusf\\|multf\\|divf\\|jmp_if\\|jmp\\|eqi\\|halt\\|swap\\|not\\|gef\\|gei\\|ret\\|call\\|native\\|andb\\|orb\\|xor\\|shr\\|shl\\|notb\\|read8\\|read16\\|read32\\|read64\\|write8\\|write16\\|write32\\|write64\\)\\>" . font-lock-keyword-face)))
 
 ;;;###autoload
 (define-derived-mode basm-mode fundamental-mode "basm"
