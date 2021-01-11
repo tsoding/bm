e267abf6ce62ffea32da262af524bff5efaab250
rexim <reximkut@gmail.com>
2021-01-11 18:25:00 +0700

    Add TODO(#84)

--- src/bdb.c
+++ src/bdb.c
@@ -305,7 +305,7 @@ Bdb_Err bdb_parse_label_or_addr(Bdb_State *st, const char *in, Inst_Addr *out)
 }
 
 /*
- * TODO: support for native function in the debugger
+ * TODO(#84): support for native function in the debugger
  * TODO: there is no way to examine the memory in bdb
  * TODO: using String_View for parsing in bdb
  */
