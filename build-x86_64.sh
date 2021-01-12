56b6cb852f297a60c516c7228b5f3026ed55639a
rexim <reximkut@gmail.com>
2021-01-12 21:45:03 +0700

    Introduce build-x86_64.sh

---
+++ build-x86_64.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -xe
+
+./build.sh
+
+./basm2nasm ./examples/123i.basm > ./examples/123i.asm
+nasm -felf64 -g ./examples/123i.asm -o ./examples/123i.o
+ld -o ./examples/123i.exe ./examples/123i.o
+
+./basm2nasm ./examples/fib.basm > ./examples/fib.asm
+nasm -felf64 -g ./examples/fib.asm -o ./examples/fib.o
+ld -o ./examples/fib.exe ./examples/fib.o
