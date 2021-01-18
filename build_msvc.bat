63bf298f97e688f57eac6bcea48de87fbbc5ed69
rexim <reximkut@gmail.com>
2021-01-18 15:16:23 +0700

    Try to use /Fe instead of /Fo

--- build_msvc.bat
+++ build_msvc.bat
@@ -1,18 +1,18 @@
 @echo off
 rem launch this from msvc-enabled console
 
-set CFLAGS=/std:c11 /O2 /FC /W4 /WX /wd4996 /nologo /Fo".\build\bin\"
+set CFLAGS=/std:c11 /O2 /FC /W4 /WX /wd4996 /nologo
 set LIBS=
 
 mkdir build
 mkdir build\bin
 mkdir build\examples
 
-cl.exe %CFLAGS% .\src\basm.c /link /out:.\build\bin\basm.exe
-cl.exe %CFLAGS% .\src\bme.c /link /out:.\build\bin\bme.exe
-cl.exe %CFLAGS% .\src\debasm.c /link /out:.\build\bin\debasm.exe
-cl.exe %CFLAGS% .\src\bdb.c /link /out:.\build\bin\bdb.exe
-cl.exe %CFLAGS% .\src\basm2nasm.c /link /out:.\build\bin\basm2nasm.exe
+cl.exe %CFLAGS% .\src\basm.c /Fe.\build\bin\basm.exe
+cl.exe %CFLAGS% .\src\bme.c /Fe.\build\bin\bme.exe
+cl.exe %CFLAGS% .\src\debasm.c /Fe.\build\bin\debasm.exe
+cl.exe %CFLAGS% .\src\bdb.c /Fe.\build\bin\bdb.exe
+cl.exe %CFLAGS% .\src\basm2nasm.c /Fe.\build\bin\basm2nasm.exe
 
 if "%1" == "examples" setlocal EnableDelayedExpansion && for /F "tokens=*" %%e in ('dir /b .\examples\*.basm') do (
     set name=%%e
