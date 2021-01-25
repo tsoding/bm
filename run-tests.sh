962964a601d80157d96254595e05fbbf138130df
rexim <reximkut@gmail.com>
2021-01-25 20:28:17 +0700

    Add TODO(#151)

--- run-tests.sh
+++ run-tests.sh
@@ -8,4 +8,4 @@ for example in `find examples/ -name \*.basm | sed "s/\.basm//"`; do
     ./build/bin/bmr -p "build/$example.bm" -eo "test/$example.expected.out"
 done
 
-# TODO: there is no script to run tests on Windows
+# TODO(#151): there is no script to run tests on Windows
