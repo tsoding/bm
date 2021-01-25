2c8685cb34dd03842eec06729c137b03e037e3d9
Danil Kolumbet <kolumbetko@gmail.com>
2021-01-26 01:41:41 +0200

    (#133) Add win folder to alternative linker commands

--- build_msvc-x86_64.bat
+++ build_msvc-x86_64.bat
@@ -15,9 +15,9 @@ for %%e in (123i fib chars hello) do (
     rem Instead of ld you can use MSVC linker:
     rem link.exe /ENTRY:_start /SUBSYSTEM:CONSOLE /MACHINE:X64 ^
         rem /defaultlib:Kernel32.lib /LargeAddressAware:NO /nologo ^
-        rem /OUT:.\build\examples\%%e.exe .\build\examples\%%e.obj
+        rem /OUT:.\build\examples\win\%%e.exe .\build\examples\win\%%e.obj
 
     rem Instead of ld you can use Go linker (http://www.godevtool.com/#linker):
-    rem GoLink.exe /entry _start /console /o .\build\examples\%%e.exe ^
-        rem .\build\examples\%%e.obj %SystemRoot%\System32\kernel32.dll
+    rem GoLink.exe /entry _start /console /o .\build\examples\win\%%e.exe ^
+        rem .\build\examples\win\%%e.obj %SystemRoot%\System32\kernel32.dll
 )
