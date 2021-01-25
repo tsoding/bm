99a6b8c443e7b08999a39e8e7d711575964eb937
Danil Kolumbet <kolumbetko@gmail.com>
2021-01-26 01:51:30 +0200

    (#113) Replace mingw linker with msvc linker

--- build_msvc-x86_64.bat
+++ build_msvc-x86_64.bat
@@ -9,15 +9,17 @@ for %%e in (123i fib chars hello) do (
     .\build\bin\basm2nasm.exe -fwin64 .\examples\%%e.basm > .\build\examples\win\%%e.asm
     
     nasm.exe -fwin64 -g .\build\examples\win\%%e.asm -o .\build\examples\win\%%e.obj
-    
-    ld.exe -entry=_start --subsystem=console -o .\build\examples\win\%%e.exe .\build\examples\win\%%e.obj %SystemRoot%\System32\kernel32.dll
 
-    rem Instead of ld you can use MSVC linker:
-    rem link.exe /ENTRY:_start /SUBSYSTEM:CONSOLE /MACHINE:X64 ^
-        rem /defaultlib:Kernel32.lib /LargeAddressAware:NO /nologo ^
-        rem /OUT:.\build\examples\win\%%e.exe .\build\examples\win\%%e.obj
+    rem MSVC linker:
+    link.exe /ENTRY:_start /SUBSYSTEM:CONSOLE /MACHINE:X64 ^
+        /defaultlib:Kernel32.lib /LargeAddressAware:NO /nologo ^
+        /OUT:.\build\examples\win\%%e.exe .\build\examples\win\%%e.obj
 
-    rem Instead of ld you can use Go linker (http://www.godevtool.com/#linker):
+    rem MinGW64 linker:
+    rem ld.exe -entry=_start --subsystem=console -o .\build\examples\win\%%e.exe ^
+        rem .\build\examples\win\%%e.obj %SystemRoot%\System32\kernel32.dll
+
+    rem Go linker (http://www.godevtool.com/#linker):
     rem GoLink.exe /entry _start /console /o .\build\examples\win\%%e.exe ^
         rem .\build\examples\win\%%e.obj %SystemRoot%\System32\kernel32.dll
 )
