559816f749333d09f73ab36996e9a7ff6ab277ab
rexim <reximkut@gmail.com>
2021-01-25 20:12:05 +0700

    (#45) run tests on CI

--- record-tests.sh
+++ record-tests.sh
@@ -10,3 +10,5 @@ mkdir -p test/examples
 for example in `find examples/ -name \*.basm | sed "s/\.basm//"`; do
     ./build/bin/bmr -p "build/$example.bm" -ao "test/$example.expected.out"
 done
+
+# TODO: there is no script to record tests on Windows
--- run-tests.sh
+++ run-tests.sh
@@ -7,3 +7,5 @@ set -xe
 for example in `find examples/ -name \*.basm | sed "s/\.basm//"`; do
     ./build/bin/bmr -p "build/$example.bm" -eo "test/$example.expected.out"
 done
+
+# TODO: there is no script to run tests on Windows
