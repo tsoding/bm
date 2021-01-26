bd2a70e7fb3eabdf16cb8e290f172df024cb99dc
rexim <reximkut@gmail.com>
2021-01-26 19:52:51 +0700

    Include minirent for Windows

--- build.h
+++ build.h
@@ -13,7 +13,74 @@
 #   include <sys/types.h>
 #   include <sys/wait.h>
 #   include <unistd.h>
-#   include <dirent.h>
+#endif // _WIN32
+
+#ifdef _WIN32
+struct DIR
+{
+    HANDLE hFind;
+    WIN32_FIND_DATA data;
+    struct dirent *dirent;
+};
+
+DIR *opendir(const char *dirpath)
+{
+    assert(dirpath);
+
+    char buffer[MAX_PATH];
+    snprintf(buffer, MAX_PATH, "%s\\*", dirpath);
+
+    DIR *dir = (DIR*)calloc(1, sizeof(DIR));
+
+    dir->hFind = FindFirstFile(buffer, &dir->data);
+    if (dir->hFind == INVALID_HANDLE_VALUE) {
+        goto fail;
+    }
+
+    return dir;
+
+fail:
+    if (dir) {
+        free(dir);
+    }
+
+    return NULL;
+}
+
+struct dirent *readdir(DIR *dirp)
+{
+    assert(dirp);
+
+    if (dirp->dirent == NULL) {
+        dirp->dirent = (dirent*)calloc(1, sizeof(struct dirent));
+    } else {
+        if(!FindNextFile(dirp->hFind, &dirp->data)) {
+            return NULL;
+        }
+    }
+
+    memset(dirp->dirent->d_name, 0, sizeof(dirp->dirent->d_name));
+
+    strncpy(
+        dirp->dirent->d_name,
+        dirp->data.cFileName,
+        sizeof(dirp->dirent->d_name) - 1);
+
+    return dirp->dirent;
+}
+
+void closedir(DIR *dirp)
+{
+    assert(dirp);
+
+    FindClose(dirp->hFind);
+    if (dirp->dirent) {
+        free(dirp->dirent);
+    }
+    free(dirp);
+}
+#else
+#    include <dirent.h>
 #endif // _WIN32
 
 #ifdef _WIN32
