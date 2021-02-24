5f9006482bd2c10c7288515596240661f0fa60e8
rexim <reximkut@gmail.com>
2021-02-25 01:35:25 +0700

    Introduce tokenizer

---
+++ src/library/tokenizer.c
@@ -0,0 +1,151 @@
+#include <assert.h>
+#include <stdio.h>
+#include <ctype.h>
+#include "./tokenizer.h"
+
+static bool is_name(char x)
+{
+    return isalnum(x) || x == '_';
+}
+
+static bool is_number(char x)
+{
+    return isalnum(x) || x == '.';
+}
+
+bool tokenizer_peek(Tokenizer *tokenizer, Token *output, File_Location location)
+{
+    if (tokenizer->peek_buffer_full) {
+        if (output) {
+            *output = tokenizer->peek_buffer;
+        }
+        return true;
+    }
+
+    tokenizer->source = sv_trim_left(tokenizer->source);
+
+    if (tokenizer->source.count == 0) {
+        return false;
+    }
+
+    Token token = {0};
+    switch (*tokenizer->source.data) {
+    case '(': {
+        token.kind = TOKEN_KIND_OPEN_PAREN;
+        token.text = sv_chop_left(&tokenizer->source, 1);
+    }
+    break;
+
+    case ')': {
+        token.kind = TOKEN_KIND_CLOSING_PAREN;
+        token.text = sv_chop_left(&tokenizer->source, 1);
+    }
+    break;
+
+    case ',': {
+        token.kind = TOKEN_KIND_COMMA;
+        token.text = sv_chop_left(&tokenizer->source, 1);
+    }
+    break;
+
+    case '>': {
+        token.kind = TOKEN_KIND_GT;
+        token.text = sv_chop_left(&tokenizer->source, 1);
+    }
+    break;
+
+    case '*': {
+        token.kind = TOKEN_KIND_MULT;
+        token.text = sv_chop_left(&tokenizer->source, 1);
+    }
+    break;
+
+    case '+': {
+        token.kind = TOKEN_KIND_PLUS;
+        token.text = sv_chop_left(&tokenizer->source, 1);
+    }
+    break;
+
+    case '-': {
+        token.kind = TOKEN_KIND_MINUS;
+        token.text = sv_chop_left(&tokenizer->source, 1);
+    }
+    break;
+
+    case '"': {
+        sv_chop_left(&tokenizer->source, 1);
+
+        size_t index = 0;
+
+        if (sv_index_of(tokenizer->source, '"', &index)) {
+            String_View text = sv_chop_left(&tokenizer->source, index);
+            sv_chop_left(&tokenizer->source, 1);
+            token.kind = TOKEN_KIND_STR;
+            token.text = text;
+        } else {
+            fprintf(stderr, FL_Fmt": ERROR: Could not find closing \"\n",
+                    FL_Arg(location));
+            exit(1);
+        }
+    }
+    break;
+
+    case '\'': {
+        sv_chop_left(&tokenizer->source, 1);
+
+        size_t index = 0;
+
+        if (sv_index_of(tokenizer->source, '\'', &index)) {
+            String_View text = sv_chop_left(&tokenizer->source, index);
+            sv_chop_left(&tokenizer->source, 1);
+            token.kind = TOKEN_KIND_CHAR;
+            token.text = text;
+        } else {
+            fprintf(stderr, FL_Fmt": ERROR: Could not find closing \'\n",
+                    FL_Arg(location));
+            exit(1);
+        }
+    }
+    break;
+
+    default: {
+        if (isalpha(*tokenizer->source.data)) {
+            token.kind = TOKEN_KIND_NAME;
+            token.text = sv_chop_left_while(&tokenizer->source, is_name);
+        } else if (isdigit(*tokenizer->source.data)) {
+            token.kind = TOKEN_KIND_NUMBER;
+            token.text = sv_chop_left_while(&tokenizer->source, is_number);
+        } else {
+            fprintf(stderr, FL_Fmt": ERROR: Unknown token starts with %c\n",
+                    FL_Arg(location), *tokenizer->source.data);
+            exit(1);
+        }
+    }
+    }
+
+    tokenizer->peek_buffer_full = true;
+    tokenizer->peek_buffer = token;
+
+    if (output) {
+        *output = token;
+    }
+
+    return true;
+}
+
+bool tokenizer_next(Tokenizer *tokenizer, Token *token, File_Location location)
+{
+    if (tokenizer_peek(tokenizer, token, location)) {
+        tokenizer->peek_buffer_full = false;
+        return true;
+    }
+
+    return false;
+}
+
+Tokenizer tokenizer_from_sv(String_View source)
+{
+    return (Tokenizer) {
+        .source = source
+    };
+}
---
+++ src/library/tokenizer.h
@@ -0,0 +1,48 @@
+#ifndef TOKENIZER_H_
+#define TOKENIZER_H_
+
+#include "sv.h"
+
+typedef struct {
+    String_View file_path;
+    int line_number;
+} File_Location;
+
+#define FL_Fmt SV_Fmt":%d"
+#define FL_Arg(location) SV_Arg(location.file_path), location.line_number
+
+typedef enum {
+    TOKEN_KIND_STR,
+    TOKEN_KIND_CHAR,
+    TOKEN_KIND_PLUS,
+    TOKEN_KIND_MINUS,
+    TOKEN_KIND_MULT,
+    TOKEN_KIND_NUMBER,
+    TOKEN_KIND_NAME,
+    TOKEN_KIND_OPEN_PAREN,
+    TOKEN_KIND_CLOSING_PAREN,
+    TOKEN_KIND_COMMA,
+    TOKEN_KIND_GT,
+} Token_Kind;
+
+const char *token_kind_name(Token_Kind kind);
+
+typedef struct {
+    Token_Kind kind;
+    String_View text;
+} Token;
+
+#define Token_Fmt "%s"
+#define Token_Arg(token) token_kind_name((token).kind)
+
+typedef struct {
+    String_View source;
+    Token peek_buffer;
+    bool peek_buffer_full;
+} Tokenizer;
+
+bool tokenizer_next(Tokenizer *tokenizer, Token *token, File_Location location);
+bool tokenizer_peek(Tokenizer *tokenizer, Token *token, File_Location location);
+Tokenizer tokenizer_from_sv(String_View source);
+
+#endif // TOKENIZER_H_
