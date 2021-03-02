feb4720a181bea57b771fce25f552ba27291e87e
rexim <reximkut@gmail.com>
2021-03-02 20:05:35 +0700

    Improve command line arguments of basm

--- src/toolchain/basm.c
+++ src/toolchain/basm.c
@@ -1,5 +1,6 @@
 #include "./basm.h"
 #include "./bm.h"
+#include "./path.h"
 
 static char *shift(int *argc, char ***argv)
 {
@@ -12,7 +13,11 @@ static char *shift(int *argc, char ***argv)
 
 static void usage(FILE *stream, const char *program)
 {
-    fprintf(stream, "Usage: %s [-g] [-I <include-path>...] -i <input.basm> -o <output.bm>\n", program);
+    fprintf(stream, "Usage: %s [OPTIONS] <input.basm>\n", program);
+    fprintf(stream, "OPTIONS:\n");
+    fprintf(stream, "    -g                    Generate symbol table\n");
+    fprintf(stream, "    -I <include/path/>    Add include path\n");
+    fprintf(stream, "    -o <output.bm>        Provide output path\n");
 }
 
 static char *get_flag_value(int *argc, char ***argv,
@@ -38,22 +43,40 @@ int main(int argc, char **argv)
     const char *input_file_path = NULL;
     const char *output_file_path = NULL;
 
+    // TODO: separate flag for basm to use new AST translation mechanism
+
     while (argc > 0) {
         const char *flag = shift(&argc, &argv);
 
-        if (strcmp(flag, "-i") == 0) {
-            input_file_path = get_flag_value(&argc, &argv, flag, program);
-        } else if (strcmp(flag, "-o") == 0) {
+        if (strcmp(flag, "-o") == 0) {
             output_file_path = get_flag_value(&argc, &argv, flag, program);
         } else if (strcmp(flag, "-g") == 0) {
             have_symbol_table = true;
         } else if (strcmp(flag, "-I") == 0) {
             basm_push_include_path(&basm, sv_from_cstr(get_flag_value(&argc, &argv, flag, program)));
         } else {
-            usage(stderr, program);
-            fprintf(stderr, "ERROR: unknown flag `%s`\n", flag);
-            exit(1);
+            if (input_file_path != NULL) {
+                usage(stderr, program);
+                fprintf(stderr, "ERROR: input file is already provided as `%s`. Only a single input file is not supported\n", input_file_path);
+                exit(1);
+            }
+            input_file_path = flag;
         }
+    }
+
+    if (input_file_path == NULL) {
+        usage(stderr, program);
+        fprintf(stderr, "ERROR: no input file is provided\n");
+        exit(1);
+    }
+
+    if (output_file_path == NULL) {
+        const String_View output_file_path_sv =
+            SV_CONCAT(&basm.arena,
+                      SV("./"),
+                      file_name_of_path(input_file_path),
+                      SV(".bm"));
+        output_file_path = arena_sv_to_cstr(&basm.arena, output_file_path_sv);
     }
 
     basm_translate_source_file(&basm, sv_from_cstr(input_file_path));
