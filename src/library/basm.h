ed0820e674ccda9eef9e25615e41673d06609913
rexim <reximkut@gmail.com>
2021-04-21 17:54:44 +0700

    Add TODO(#326)

--- src/library/basm.h
+++ src/library/basm.h
@@ -166,7 +166,9 @@ bool basm_resolve_include_file_path(Basm *basm,
                                     String_View file_path,
                                     String_View *resolved_path);
 Macrodef *basm_resolve_macrodef(Basm *basm, String_View name);
-void basm_translate_macro_call(Basm *basm, Macrocall_Statement macrocall, File_Location location);
+// TODO(#326): all the `basm_translate_*` functions that translate statements should have the name `basm_translate_<statement-type-in-lower-case>`
+// Like `basm_translate_macrocall_statement` or `basm_translate_macrodef_statement`
+void basm_translate_macrocall_statement(Basm *basm, Macrocall_Statement macrocall, File_Location location);
 void basm_translate_macrodef_statement(Basm *basm, Macrodef_Statement macrodef, File_Location location);
 void basm_translate_block(Basm *basm, Block_Statement *block);
 void basm_translate_bind_const(Basm *basm, Bind_Const_Statement bind_const, File_Location location);
