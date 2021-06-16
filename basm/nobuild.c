7351cc2d455aa1d134e62821a0dd9a552f051900
rexim <reximkut@gmail.com>
2021-06-17 02:18:20 +0700

    Add basm to CI

--- basm/nobuild.c
+++ basm/nobuild.c
@@ -4,26 +4,26 @@
 
 #define CFLAGS COMMON_FLAGS
 
-#define INCLUDES "-I../common/", \
-                 "-I../bm/src/"
+#define INCLUDES INCLUDE_FLAG(PATH("..", "common")), \
+                 INCLUDE_FLAG(PATH("..", "bm", "src"))
 
-#define COMMON_UNITS "../common/sv.c", \
-                     "../common/arena.c", \
-                     "../common/path.c"
+#define COMMON_UNITS PATH("..", "common", "sv.c"), \
+                     PATH("..", "common", "arena.c"), \
+                     PATH("..", "common", "path.c")
 
-#define BM_UNITS     "../bm/src/types.c", \
-                     "../bm/src/bm.c"
+#define BM_UNITS     PATH("..", "bm", "src", "types.c"), \
+                     PATH("..", "bm", "src", "bm.c")
 
-#define BASM_UNITS   "./src/compiler.c", \
-                     "./src/expr.c", \
-                     "./src/fl.c", \
-                     "./src/gas_arm64.c", \
-                     "./src/linizer.c", \
-                     "./src/nasm_sysv_x86_64.c", \
-                     "./src/statement.c", \
-                     "./src/target.c", \
-                     "./src/tokenizer.c", \
-                     "./src/verifier.c"
+#define BASM_UNITS   PATH("src", "compiler.c"), \
+                     PATH("src", "expr.c"), \
+                     PATH("src", "fl.c"), \
+                     PATH("src", "gas_arm64.c"), \
+                     PATH("src", "linizer.c"), \
+                     PATH("src", "nasm_sysv_x86_64.c"), \
+                     PATH("src", "statement.c"), \
+                     PATH("src", "target.c"), \
+                     PATH("src", "tokenizer.c"), \
+                     PATH("src", "verifier.c")
 
 #define UNITS COMMON_UNITS, BM_UNITS, BASM_UNITS
 
@@ -32,10 +32,11 @@
 int main()
 {
     MKDIRS("bin");
-    CMD("cc", CFLAGS, INCLUDES, "-o", "./bin/basm", UNITS, "./src/basm.c", LIBS);
-    CMD("cc", CFLAGS, INCLUDES, "-o", "./bin/basm2dot", UNITS, "./src/basm2dot.c", LIBS);
-    CMD("cc", CFLAGS, INCLUDES, "-o", "./bin/expr2dot", UNITS, "./src/expr2dot.c", LIBS);
+    CC("bin", "basm", PATH("src", "basm.c"));
+    CC("bin", "basm2dot", PATH("src", "basm2dot.c"));
+    CC("bin", "expr2dot", PATH("src", "expr2dot.c"));
 
     // TODO: test is not implemented
+
     return 0;
 }
