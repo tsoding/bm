17f3a72e99e56695053683059955c2d3df03a065
rexim <reximkut@gmail.com>
2021-06-17 02:27:26 +0700

    Add bang build to CI

--- bang/nobuild.c
+++ bang/nobuild.c
@@ -4,34 +4,33 @@
 
 #define CFLAGS COMMON_FLAGS
 
-#define INCLUDES "-I../common/", \
-                 "-I../bm/src/", \
-                 "-I../basm/src/"
-#define COMMON_UNITS "../common/sv.c", \
-                     "../common/arena.c", \
-                     "../common/path.c"
-#define BM_UNITS   "../bm/src/types.c", \
-                   "../bm/src/bm.c"
-#define BASM_UNITS "../basm/src/compiler.c", \
-                   "../basm/src/expr.c", \
-                   "../basm/src/fl.c", \
-                   "../basm/src/gas_arm64.c", \
-                   "../basm/src/linizer.c", \
-                   "../basm/src/nasm_sysv_x86_64.c", \
-                   "../basm/src/statement.c", \
-                   "../basm/src/target.c", \
-                   "../basm/src/tokenizer.c", \
-                   "../basm/src/verifier.c"
-#define BANG_UNITS "./src/bang.c", \
-                   "./src/bang_compiler.c", \
-                   "./src/bang_lexer.c", \
-                   "./src/bang_parser.c"
+#define INCLUDES INCLUDE_FLAG(PATH("..", "common")), \
+                 INCLUDE_FLAG(PATH("..", "bm", "src")), \
+                 INCLUDE_FLAG(PATH("..", "basm", "src"))
+#define COMMON_UNITS PATH("..", "common", "sv.c"), \
+                     PATH("..", "common", "arena.c"), \
+                     PATH("..", "common", "path.c")
+#define BM_UNITS   PATH("..", "bm", "src", "types.c"), \
+                   PATH("..", "bm", "src", "bm.c")
+#define BASM_UNITS PATH("..", "basm", "src", "compiler.c"), \
+                   PATH("..", "basm", "src", "expr.c"), \
+                   PATH("..", "basm", "src", "fl.c"), \
+                   PATH("..", "basm", "src", "gas_arm64.c"), \
+                   PATH("..", "basm", "src", "linizer.c"), \
+                   PATH("..", "basm", "src", "nasm_sysv_x86_64.c"), \
+                   PATH("..", "basm", "src", "statement.c"), \
+                   PATH("..", "basm", "src", "target.c"), \
+                   PATH("..", "basm", "src", "tokenizer.c"), \
+                   PATH("..", "basm", "src", "verifier.c")
+#define BANG_UNITS PATH("src", "bang_compiler.c"), \
+                   PATH("src", "bang_lexer.c"), \
+                   PATH("src", "bang_parser.c")
 #define UNITS BM_UNITS, COMMON_UNITS, BASM_UNITS, BANG_UNITS
 #define LIBS "-lm"
 
 int main()
 {
     MKDIRS("bin");
-    CMD("cc", CFLAGS, INCLUDES, "-o", "bin/bang", UNITS, LIBS);
+    CC("bin", "bang", PATH("src", "bang.c"));
     return 0;
 }
