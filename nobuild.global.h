ede71e42ccff92af9753fe2f08dd587b262f9ac1
rexim <reximkut@gmail.com>
2021-06-17 01:37:26 +0700

    Get rid of the root nobuild

--- nobuild.global.h
+++ nobuild.global.h
@@ -19,6 +19,16 @@
             UNITS, \
             entry_unit); \
     } while(0)
+#define RUN_NOBUILD_NOARGS \
+    do { \
+        CMD("cl.exe", CFLAGS, "nobuild.c"); \
+        CMD(".\nobuild.exe"); \
+    } while(0)
+#define RUN_NOBUILD(...) \
+    do { \
+        CMD("cl.exe", CFLAGS, "nobuild.c"); \
+        CMD(".\nobuild.exe", __VA_ARGS__); \
+    } while(0)
 #else
 #define INCLUDE_FLAG(path) CONCAT("-I", (path))
 #define CC(out_dir, out_name, entry_unit) \
@@ -29,6 +39,34 @@
         } \
         CMD(cc, CFLAGS, INCLUDES, "-o", PATH(out_dir, out_name), UNITS, entry_unit, LIBS); \
     } while(0)
+#define RUN_NOBUILD_NOARGS \
+    do { \
+        const char *cc = getenv("CC"); \
+        if (cc == NULL) { \
+            cc = "cc"; \
+        } \
+        CMD(cc, CFLAGS, "-o", "nobuild", "nobuild.c"); \
+        CMD("./nobuild"); \
+    } while(0)
+#define RUN_NOBUILD(...) \
+    do { \
+        const char *cc = getenv("CC"); \
+        if (cc == NULL) { \
+            cc = "cc"; \
+        } \
+        CMD(cc, CFLAGS, "-o", "nobuild", "nobuild.c"); \
+        CMD("./nobuild", __VA_ARGS__); \
+    } while(0)
 #endif // _WIN32
+
+inline static void astyle_format_folder(const char *folder_path) {
+    FOREACH_FILE_IN_DIR(file, folder_path, {
+        if (ENDS_WITH(file, ".c") || ENDS_WITH(file, ".h"))
+        {
+            const char *file_path = PATH(folder_path, file);
+            CMD("astyle", "--style=kr", file_path);
+        }
+    });
+}
 
 #endif // NOBUILD_GLOBAL_H_
