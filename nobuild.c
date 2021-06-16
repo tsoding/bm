43873645de0b460694387944192fbd67b2ba9f2d
rexim <reximkut@gmail.com>
2021-06-17 02:44:03 +0700

    Introduce root nobuild

---
+++ nobuild.c
@@ -0,0 +1,28 @@
+#define NOBUILD_IMPLEMENTATION
+#include "./nobuild.h"
+#include "./nobuild.global.h"
+
+static const char *const subdirs[] = {
+    "bm", "debasm", "bdb", "basm", "bang"
+};
+static const size_t subdirs_count = sizeof(subdirs) / sizeof(subdirs[0]);
+
+int main()
+{
+    for (size_t i = 0; i < subdirs_count; ++i) {
+        INFO("------------------------------");
+        INFO("SUBDIR: %s", subdirs[i]);
+        INFO("------------------------------");
+        chdir(subdirs[i]);
+#ifdef _WIN32
+        CMD("cl.exe", "nobuild.c");
+        CMD(".\\nobuild.exe");
+#else
+        CMD("cc", "-o", "nobuild", "nobuild.c");
+        CMD("./nobuild");
+#endif
+        chdir("..");
+    }
+
+    return 0;
+}
