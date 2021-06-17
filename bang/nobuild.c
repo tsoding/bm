9c9990cbcb2f2d8a02232cf52ddc47b13c511af3
rexim <reximkut@gmail.com>
2021-06-18 01:51:27 +0700

    Add TODO(#460)

--- bang/nobuild.c
+++ bang/nobuild.c
@@ -30,7 +30,7 @@
 
 int main()
 {
-    // TODO: there is no testing in Bang subproject
+    // TODO(#460): there is no testing in Bang subproject
     MKDIRS("bin");
     CC("bin", "bang", PATH("src", "bang.c"));
     return 0;
