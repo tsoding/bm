22c79a431544e7fb668f79a4a15ed906bcfdab7f
rexim <reximkut@gmail.com>
2021-06-18 01:24:51 +0700

    (#451) Make the root nobuild pass the args to the child nobuilds

--- nobuild.c
+++ nobuild.c
@@ -9,7 +9,7 @@ static const size_t subdirs_count = sizeof(subdirs) / sizeof(subdirs[0]);
 
 // TODO(#452): the root nobuild does not have the fmt subcommand
 
-int main()
+int main(int argc, char **argv)
 {
     for (size_t i = 0; i < subdirs_count; ++i) {
         INFO("------------------------------");
@@ -18,11 +18,18 @@ int main()
         chdir(subdirs[i]);
 #ifdef _WIN32
         CMD("cl.exe", "nobuild.c");
-        CMD(".\\nobuild.exe");
+        Cstr_Array nobuild_args = cstr_array_make(".\\nobuild.exe", NULL);
 #else
         CMD("cc", "-o", "nobuild", "nobuild.c");
-        CMD("./nobuild");
+        Cstr_Array nobuild_args = cstr_array_make("./nobuild", NULL);
 #endif
+        for (int i = 1; i < argc; ++i) {
+            nobuild_args = cstr_array_append(nobuild_args, argv[i]);
+        }
+        Cmd nobuild_cmd = {nobuild_args};
+        INFO("CMD: %s", cmd_show(nobuild_cmd));
+        cmd_run_sync(nobuild_cmd);
+
         chdir("..");
     }
 
