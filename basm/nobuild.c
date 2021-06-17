d2804ca37fc103e3f0a1834b48858f3635a6e0bc
rexim <reximkut@gmail.com>
2021-06-18 01:07:07 +0700

    (#451) Implement testing for basm

--- basm/nobuild.c
+++ basm/nobuild.c
@@ -1,3 +1,5 @@
+#include <stdbool.h>
+
 #define NOBUILD_IMPLEMENTATION
 #include "../nobuild.h"
 #include "../nobuild.global.h"
@@ -29,14 +31,53 @@
 
 #define LIBS "-lm"
 
-int main()
+void basm_test(bool record)
 {
     MKDIRS("bin");
     CC("bin", "basm", PATH("src", "basm.c"));
-    CC("bin", "basm2dot", PATH("src", "basm2dot.c"));
-    CC("bin", "expr2dot", PATH("src", "expr2dot.c"));
 
-    // TODO(#451): basm tests are not implemented
+    const char *bmr_path = PATH("..", "bm", "bin", "bmr");
+
+    if (!PATH_EXISTS(bmr_path)) {
+        PANIC("Could not find `%s` please go to the bm subproject and build it",
+              bmr_path);
+    }
+
+    MKDIRS("bin", "test", "cases");
+    FOREACH_FILE_IN_DIR(example, PATH("test", "cases"), {
+        if (ENDS_WITH(example, ".basm"))
+        {
+            const char *expected_output_path = PATH("test", "outputs", CONCAT(NOEXT(example), ".expected.out"));
+            const char *bm_path = PATH("bin", "test", "cases", CONCAT(NOEXT(example), ".bm"));
+
+            CMD(PATH("bin", "basm"),
+                "-I", PATH("lib"),
+                "-o", bm_path,
+                PATH("test", "cases", example));
+
+            CMD(bmr_path,
+                "-p", bm_path,
+                record ? "-ao" : "-eo", expected_output_path);
+        }
+    });
+}
+
+int main(int argc, char **argv)
+{
+    if (argc >= 2) {
+        if (strcmp(argv[1], "test") == 0) {
+            basm_test(false);
+        } else if (strcmp(argv[1], "record") == 0) {
+            basm_test(true);
+        } else {
+            PANIC("unknown subcommand `%s`", argv[1]);
+        }
+    } else {
+        MKDIRS("bin");
+        CC("bin", "basm", PATH("src", "basm.c"));
+        CC("bin", "basm2dot", PATH("src", "basm2dot.c"));
+        CC("bin", "expr2dot", PATH("src", "expr2dot.c"));
+    }
 
     return 0;
 }
