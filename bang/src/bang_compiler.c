0da0758539f8d26a89926914142a23bcc4df06de
rexim <reximkut@gmail.com>
2021-06-17 19:54:26 +0700

    Add TODO(#458)

--- bang/src/bang_compiler.c
+++ bang/src/bang_compiler.c
@@ -750,7 +750,7 @@ void compile_stack_var_def_into_basm(Bang *bang, Basm *basm, Bang_Var_Def var_de
         exit(1);
     }
 
-    assert(false && "TODO: compiling the stack variable is not implemented");
+    assert(false && "TODO(#458): compiling the stack variable is not implemented");
 }
 
 void bang_push_new_scope(Bang *bang)
